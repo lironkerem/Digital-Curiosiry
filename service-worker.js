a099aa9b07083cf6d2492936f1e475d1d835e357
lironkerem <lironkerem@gmail.com>
2025-12-16 11:18:39 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'dc-v1';
 const urlsToCache = [
-  '/Digital-Curiosity/',
-  '/Digital-Curiosity/Index.html',
-  '/Digital-Curiosity/Assets/CSS/champagne-gold.css',
-  '/Digital-Curiosity/Assets/CSS/dark-mode.css',
-  '/Digital-Curiosity/Core/Index.js',
-  '/Digital-Curiosity/Core/ProjectCuriosityApp.js',
-  '/Digital-Curiosity/Icons/icon-192x192.png',
-  '/Digital-Curiosity/Icons/icon-512x512.png'
+  '/Digital-Curiosiry/',
+  '/Digital-Curiosiry/Index.html',
+  '/Digital-Curiosiry/Assets/CSS/champagne-gold.css',
+  '/Digital-Curiosiry/Assets/CSS/dark-mode.css',
+  '/Digital-Curiosiry/Core/Index.js',
+  '/Digital-Curiosiry/Core/ProjectCuriosityApp.js',
+  '/Digital-Curiosiry/Icons/icon-192x192.png',
+  '/Digital-Curiosiry/Icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
