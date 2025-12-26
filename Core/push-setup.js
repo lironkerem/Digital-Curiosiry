39bd4619332949ef6e91ebd5f809be8979862945
lironkerem <lironkerem@gmail.com>
2025-12-26 12:15:02 +0200

    Update push-setup.js

--- Core/push-setup.js
+++ Core/push-setup.js
@@ -1,34 +1,5 @@
 // Core/push-setup.js
-const VAPID_PUBLIC_KEY = 'BGC3GSs75wSk-IXvSHfsmr725CJnQxNuYJHExJZ113yITzwPgAZrVe6-IGyD1zC_t5mtH3-HG1P4GndS8PnSrOc';
-
-const pushBtn = document.getElementById('pushBtn');
-if (!pushBtn) throw new Error('pushBtn not found');
-
-pushBtn.onclick = async () => {
-  const sw = await navigator.serviceWorker.ready;
-  const sub = await sw.pushManager.getSubscription();
-  if (sub) {
-    await sub.unsubscribe();
-    pushBtn.textContent = 'Enable notifications';
-  } else {
-    const newSub = await sw.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlB64ToUint8Array(VAPID_PUBLIC_KEY)
-    });
-    await fetch('/api/save-sub', {
-      method: 'POST',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify(newSub)
-    });
-    pushBtn.textContent = 'Notifications ON';
-  }
+document.getElementById('pushBtn').onclick = () => {
+  alert('Handler attached');
+  document.getElementById('pushBtn').textContent = 'Notifications ON';
 };
-
-function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) outputArray[i] = rawData.charCodeAt(i);
-  return outputArray;
-}
