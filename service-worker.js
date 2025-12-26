20132060daa924d6acb7b8264ee5468a4ec83d29
lironkerem <lironkerem@gmail.com>
2025-12-26 10:54:27 +0200

    Web Push

--- service-worker.js
+++ service-worker.js
@@ -21,3 +21,24 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
+// PUSH NOTIFICATION LISTENER
+self.addEventListener('push', event => {
+  const { title, body, icon, tag, data } = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body,
+      icon: icon || '/Icons/icon-192x192.png',
+      tag: tag || 'default',
+      data: data || {},
+      badge: '/Icons/icon-192x192.png',
+      vibrate: [200, 100, 200]
+    })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  const urlToOpen = event.notification.data?.url || '/';
+  event.waitUntil(clients.openWindow(urlToOpen));
+});
