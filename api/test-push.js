bf86c53cc6cddf019544aa7be82076750ffc99f9
lironkerem <lironkerem@gmail.com>
2025-12-26 12:34:23 +0200

    test push

--- api/test-push.js
+++ api/test-push.js
@@ -1,3 +1,7 @@
+// api/test-push.js
+export default (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`
 <!doctype html>
 <html>
 <head>
@@ -7,21 +11,17 @@
 </head>
 <body>
   <h2>Send yourself a test notification</h2>
-  <ol>
-    <li>Open your real app in another tab, click <b>Enable notifications</b> and allow the browser prompt.</li>
-    <li>Come back here and press the button below.</li>
-  </ol>
   <button id="fire">Fire test notification</button>
   <pre id="log"></pre>
 
   <script>
-    const log = txt => document.getElementById('log').textContent += txt + '\n';
+    const log = txt => document.getElementById('log').textContent += txt + '\\n';
     fire.onclick = async () => {
-      log('Requesting sub list from /api/subs...');
+      log('Fetching subscriptions...');
       const res = await fetch('/api/subs');
       const list = await res.json();
       if (!list.length) return log('No subscriptions saved yet. Enable notifications in your app first.');
-      log(`Found ${list.length} sub(s). Sending...`);
+      log('Found ' + list.length + ' sub(s). Sending...');
       const send = await fetch('/api/send', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
@@ -36,3 +36,5 @@
   </script>
 </body>
 </html>
+  `);
+};
